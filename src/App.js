27c67ace6ed0c67b74a500e13f89b7d9c7f251c5
najinp <najinida92@gmail.com>
2022-10-04 17:50:39 +0900

    20221004

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import './css/reset.css'
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './css/Header.scss';
+
+const Header = () => {
+    return (
+
+        <nav className='Gnb'>
+            <ul>
+
+            </ul>
+            <h1>
+                <img src='public/assets/img/logo512.png' alt="" />
+            </h1>
+            <ul>
+
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Header
